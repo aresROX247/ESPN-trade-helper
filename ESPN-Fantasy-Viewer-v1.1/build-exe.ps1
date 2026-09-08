41d8d4ee1ec9a5e78a10386fea97890deefef33a
wyatt barnes <56194893+aresROX247@users.noreply.github.com>
2026-09-07 21:33:51 -0400

    Delete ESPN-Fantasy-Viewer-v1.1 directory

--- ESPN-Fantasy-Viewer-v1.1/build-exe.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$projectPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sourcePath = Join-Path $projectPath 'ESPNFantasyImporter.cs'
-$outputPath = Join-Path $projectPath 'ESPN Fantasy Importer.exe'
-
-Add-Type -TypeDefinition (Get-Content -Raw $sourcePath) -OutputAssembly $outputPath -OutputType ConsoleApplication
-Write-Host "Created: $outputPath"
